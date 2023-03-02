32000f129d8be5bc3239ce9ad35f29d63f9c31ab
ro0k23 <125355361+ro0k23@users.noreply.github.com>
2023-03-02 09:37:15 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showNumber(input.compassHeading())
+})
+basic.forever(function () {
+	
+})
